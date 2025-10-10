b10416e5f2efbce3887f213e5ccbdd0caa101619
Daniel Di Domenico <daniel.domenico@ifpr.edu.br>
2025-10-10 18:55:27 -0300

    Implementação MVC Alunos

---
+++ implementacoes/mvc_alunos/view/alunos/listar.php
@@ -0,0 +1,21 @@
+<h3>Listagem de Alunos</h3> 
+
+<div>
+    <a href="inserir.php">Inserir</a>
+</div>
+
+<table>
+    <!-- Cabeçalho -->
+    <tr>
+        <th>ID</th>
+        <th>Nome</th>
+        <th>Idade</th>
+        <th>Estrangeiro</th>
+        <th>Curso</th>
+        <th></th>
+        <th></th>
+    </tr>
+
+    <!-- Dados -->
+   
+</table>
